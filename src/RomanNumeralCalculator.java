a6e56fe853089e935b871235ff90ee436cfe2a8e
Nicole Lefebvre <njlefebvre@gmail.com>
2025-02-03 23:09:00 -0700

    Hundreds and tens place should be populated corrected, even in conjunction with other places

--- src/RomanNumeralCalculator.java
+++ src/RomanNumeralCalculator.java
@@ -1,15 +1,55 @@
 public class RomanNumeralCalculator {
-    public static final String THOUSANDS_PLACE_SYMBOL = "M";
+    public static final String THOUSANDS_SYMBOL = "M";
+    public static final String HUNDREDS_SYMBOL = "C";
+    private static final String FIVE_HUNDRED_SYMBOL = "D";
+    public static final String TENS_SYMBOL = "X";
+    public static final String FIFTY_SYMBOL = "L";
 
     public static String determineRomanNumeral(int arabicNumber) {
-        return thousandsPlaceNumeral(arabicNumber); //+
-//                hundredsPlaceNumbera(arabicNumber) +
-//                tensPlaceNumeral(arabicNumber) +
+        return thousandsPlaceNumeral(arabicNumber) +
+                hundredsPlaceNumeral(arabicNumber) +
+                tensPlaceNumeral(arabicNumber); //+
 //                onesPlaceNumeral(arabicNumber);
+    }
+
+    private static String tensPlaceNumeral(int arabicNumber) {
+        int hundredsPlace = (arabicNumber % 100) / 10;
+
+        if (hundredsPlace >= 1 && hundredsPlace <= 3) {
+            return TENS_SYMBOL.repeat(Math.max(0, hundredsPlace));
+        } else if (hundredsPlace == 4) {
+            return TENS_SYMBOL + FIFTY_SYMBOL;
+        } else if (hundredsPlace == 5) {
+            return FIFTY_SYMBOL;
+        } else if (hundredsPlace >= 6 && hundredsPlace <= 8) {
+            return FIFTY_SYMBOL + TENS_SYMBOL.repeat(Math.max(0, hundredsPlace - 5));
+        } else if (hundredsPlace == 9) {
+            return TENS_SYMBOL + HUNDREDS_SYMBOL;
+        }
+
+        return "";
+    }
+
+    private static String hundredsPlaceNumeral(int arabicNumber) {
+        int hundredsPlace = (arabicNumber % 1000) / 100;
+
+        if (hundredsPlace >= 1 && hundredsPlace <= 3) {
+            return HUNDREDS_SYMBOL.repeat(hundredsPlace);
+        } else if (hundredsPlace == 4) {
+            return HUNDREDS_SYMBOL + FIVE_HUNDRED_SYMBOL;
+        } else if (hundredsPlace == 5) {
+            return FIVE_HUNDRED_SYMBOL;
+        } else if (hundredsPlace >= 6 && hundredsPlace <= 8) {
+            return FIVE_HUNDRED_SYMBOL + HUNDREDS_SYMBOL.repeat(Math.max(0, hundredsPlace - 5));
+        } else if (hundredsPlace == 9) {
+            return HUNDREDS_SYMBOL + THOUSANDS_SYMBOL;
+        }
+
+        return "";
     }
 
     private static String thousandsPlaceNumeral(int arabicNumber) {
         int thousandsPlace = arabicNumber / 1000;
-        return THOUSANDS_PLACE_SYMBOL.repeat(Math.max(0, thousandsPlace));
+        return THOUSANDS_SYMBOL.repeat(Math.max(0, thousandsPlace));
     }
 }
--- tests/RomanNumeralCalculatorTest.java
+++ tests/RomanNumeralCalculatorTest.java
@@ -38,7 +38,6 @@ public class RomanNumeralCalculatorTest {
         assertEquals("MMMMMMMMMM", romanNumeral);
     }
 
-
     @Test
     void determineRomanNumeral_100_returnsC() {
         String romanNumeral = RomanNumeralCalculator.determineRomanNumeral(100);
@@ -59,36 +58,42 @@ public class RomanNumeralCalculatorTest {
 
         assertEquals("CCC", romanNumeral);
     }
+
     @Test
     void determineRomanNumeral_400_returnsCD() {
         String romanNumeral = RomanNumeralCalculator.determineRomanNumeral(400);
 
         assertEquals("CD", romanNumeral);
     }
+
     @Test
     void determineRomanNumeral_500_returnsD() {
         String romanNumeral = RomanNumeralCalculator.determineRomanNumeral(500);
 
         assertEquals("D", romanNumeral);
     }
+
     @Test
     void determineRomanNumeral_600_returnsDC() {
         String romanNumeral = RomanNumeralCalculator.determineRomanNumeral(600);
 
         assertEquals("DC", romanNumeral);
     }
+
     @Test
     void determineRomanNumeral_700_returnsDCC() {
         String romanNumeral = RomanNumeralCalculator.determineRomanNumeral(700);
 
         assertEquals("DCC", romanNumeral);
     }
+
     @Test
     void determineRomanNumeral_800_returnsDCCC() {
         String romanNumeral = RomanNumeralCalculator.determineRomanNumeral(800);
 
         assertEquals("DCCC", romanNumeral);
     }
+
     @Test
     void determineRomanNumeral_900_returnsCM() {
         String romanNumeral = RomanNumeralCalculator.determineRomanNumeral(900);
@@ -96,5 +101,95 @@ public class RomanNumeralCalculatorTest {
         assertEquals("CM", romanNumeral);
     }
 
+    @Test
+    void determineRomanNumeral_1900_returnsMCM() {
+        String romanNumeral = RomanNumeralCalculator.determineRomanNumeral(1900);
+
+        assertEquals("MCM", romanNumeral);
+    }
+
+    @Test
+    void determineRomanNumeral_2400_returnsMMCD() {
+        String romanNumeral = RomanNumeralCalculator.determineRomanNumeral(2400);
+
+        assertEquals("MMCD", romanNumeral);
+    }
+
+    @Test
+    void determineRomanNumeral_10_returnsX() {
+        String romanNumeral = RomanNumeralCalculator.determineRomanNumeral(10);
+
+        assertEquals("X", romanNumeral);
+    }
+
+    @Test
+    void determineRomanNumeral_20_returnsXX() {
+        String romanNumeral = RomanNumeralCalculator.determineRomanNumeral(20);
+
+        assertEquals("XX", romanNumeral);
+    }
+
+    @Test
+    void determineRomanNumeral_30_returnsXXX() {
+        String romanNumeral = RomanNumeralCalculator.determineRomanNumeral(30);
+
+        assertEquals("XXX", romanNumeral);
+    }
+
+    @Test
+    void determineRomanNumeral_40_returnsXL() {
+        String romanNumeral = RomanNumeralCalculator.determineRomanNumeral(40);
+
+        assertEquals("XL", romanNumeral);
+    }
+
+    @Test
+    void determineRomanNumeral_50_returnsL() {
+        String romanNumeral = RomanNumeralCalculator.determineRomanNumeral(50);
+
+        assertEquals("L", romanNumeral);
+    }
+
+    @Test
+    void determineRomanNumeral_60_returnsLX() {
+        String romanNumeral = RomanNumeralCalculator.determineRomanNumeral(60);
+
+        assertEquals("LX", romanNumeral);
+    }
+
+    @Test
+    void determineRomanNumeral_70_returnsLXX() {
+        String romanNumeral = RomanNumeralCalculator.determineRomanNumeral(70);
+
+        assertEquals("LXX", romanNumeral);
+    }
+
+    @Test
+    void determineRomanNumeral_80_returnsLXXX() {
+        String romanNumeral = RomanNumeralCalculator.determineRomanNumeral(80);
+
+        assertEquals("LXXX", romanNumeral);
+    }
+
+    @Test
+    void determineRomanNumeral_90_returnsXC() {
+        String romanNumeral = RomanNumeralCalculator.determineRomanNumeral(90);
+
+        assertEquals("XC", romanNumeral);
+    }
+
+    @Test
+    void determineRomanNumeral_1990_returnsMCM() {
+        String romanNumeral = RomanNumeralCalculator.determineRomanNumeral(1990);
+
+        assertEquals("MCMXC", romanNumeral);
+    }
+
+    @Test
+    void determineRomanNumeral_2430_returnsMMCDXXX() {
+        String romanNumeral = RomanNumeralCalculator.determineRomanNumeral(2430);
+
+        assertEquals("MMCDXXX", romanNumeral);
+    }
 
 }
